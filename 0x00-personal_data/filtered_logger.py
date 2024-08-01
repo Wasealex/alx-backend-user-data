009e8f43ad7fa31b8b32e19833d681ab4eb5c0b0
Wasealex <wasealex27@gmail.com>
2024-08-01 12:17:23 +0300

    filterning message correctly on given basic information

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a module that contains filltering log functions
+"""
+import re
+from typing import List
+
+patterns = {
+    "extract": lambda fields, separator: f"({separator.join(fields)})",
+    "replace": lambda redaction: "REDACTED",
+}
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str, separator: str) -> str:
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
