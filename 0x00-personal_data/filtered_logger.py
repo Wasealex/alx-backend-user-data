b01d7387e6437557395be346262edd12a16df911
Wasealex <wasealex27@gmail.com>
2024-08-01 22:40:26 +0300

    getting db acceses yusing mysql connector

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,9 @@
 """a module that contains filltering log functions
 """
 import logging
+import os
 import re
+import mysql.connector
 from typing import List
 
 patterns = {
@@ -49,3 +51,18 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
     logger.addHandler(stream_handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Creates a connector to a database."""
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    connection = mysql.connector.connect(
+        host=db_host,
+        user=db_user,
+        password=db_pwd,
+        database=db_name,
+    )
+    return connection
