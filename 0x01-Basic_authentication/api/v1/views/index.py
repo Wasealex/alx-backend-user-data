a9bd69c5abc006650981cb955f21eeda1f730ce6
Wasealex <Wasealex27@gmail.com>
2024-08-07 12:50:52 +0000

    unauthorized access

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', method=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """unuthorized error show
+    """
+    abort(401, description='Unauthorized')
