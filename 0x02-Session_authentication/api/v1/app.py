1b93f8cd916eeb9943409dced94aec9a1d16c2bc
Wasealex <wasealex27@gmail.com>
2024-08-09 11:09:21 +0300

    pycodestyle fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,7 +38,7 @@ def before_request():
                    '/api/v1/auth_session/login/']
         if auth.require_auth(request.path, exluded):
             if auth.authorization_header(request) is None and\
-                auth.session_cookie(request) is None:
+                    auth.session_cookie(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
