3ad6d328ce08415a6d704dd6de82bffe79d00a34
Wasealex <wasealex27@gmail.com>
2024-08-09 08:54:28 +0300

    pycodestyle fixed on session_auth and created a cookie in auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,7 +18,7 @@ class SessionAuth(Auth):
         session_id = uuid4()
         self.user_id_by_session_id[str(session_id)] = user_id
         return str(session_id)
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """user_id_for_session_id method
         """
