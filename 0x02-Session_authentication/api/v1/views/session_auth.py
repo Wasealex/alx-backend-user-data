9e6424d0d680810f0196b3e23072c906f3ddcf1a
Wasealex <wasealex27@gmail.com>
2024-08-09 14:47:01 +0300

    stay logged in using the session id

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """session_auth module
 """
-from flask import jsonify, request
+from flask import abort, jsonify, request
 from api.v1.views import app_views
 from models.user import User
 from os import getenv
@@ -30,3 +30,16 @@ def auth_session_login() -> str:
             response.set_cookie(getenv("SESSION_NAME"), session_id)
             return response
     return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def auth_session_logout() -> str:
+    """ DELETE /auth_session/logout
+    Return:
+      - empty JSON payload
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
