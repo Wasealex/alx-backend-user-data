e070c3a8bd3d3322118ec894b52aa9d31e0f91de
Wasealex <wasealex27@gmail.com>
2024-08-14 18:25:04 +0300

    cracked the db for invalid and not found

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,20 +3,19 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
+from sqlalchemy.orm import sessionmaker 
 from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.orm.session import Session
 from user import Base, User
 
 
 class DB:
     """DB class
     """
-
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine('sqlite:///a.db', echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -31,23 +30,24 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """adds user to the session returns User"""
+        """add_user
+        """
         if not email or not hashed_password:
-            return
+            return None
         user = User(email=email, hashed_password=hashed_password)
         session = self._session
         session.add(user)
         session.commit()
         return user
-    
+
     def find_user_by(self, **kwargs) -> User:
-        """find user by filtering the db
+        """find_user_by
         """
-        if not kwargs:
+        if not kwargs or not kwargs.keys():
             raise InvalidRequestError
-        
         session = self._session
         try:
             return session.query(User).filter_by(**kwargs).one()
         except Exception:
             raise NoResultFound
+        
