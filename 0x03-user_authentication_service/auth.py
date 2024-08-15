81d71c191e5376fd076c6e974b64c2c19c06e3ac
Wasealex <wasealex27@gmail.com>
2024-08-15 15:03:45 +0300

    modify destroy using find user rather update to none

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -71,5 +71,10 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """Destroy a session
         """
-        self._db.update_user(user_id, session_id=None)
-        return
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        else:
+            user.session_id = None
+            return None
