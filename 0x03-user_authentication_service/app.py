5d7dde6015f1270c6d8c48f3f5a85fb82589955f
Wasealex <wasealex27@gmail.com>
2024-08-15 14:25:16 +0300

    logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,5 +52,20 @@ def login() -> str:
         abort(401)
 
 
+def logout() -> str:
+    """DELETE /sessions
+    Logout
+    Return:
+      - message
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return message()
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
