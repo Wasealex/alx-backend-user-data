8bd95c3d34accea423eeee1a4d0a7501579e6081
Wasealex <wasealex27@gmail.com>
2024-08-15 15:19:41 +0300

    pol

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -61,10 +61,10 @@ def logout() -> str:
     """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if not user:
-        abort(403)
-    AUTH.destroy_session(user.id)
-    return jsonify({"message": "Bienvenue"})
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect("/")
+    abort(403)
 
 
 if __name__ == "__main__":
